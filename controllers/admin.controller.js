d5ad44f7f0a6085a44c480b68f84a08c5b97faaa
binalDoc <binal.p@docfliq.com>
2026-04-01 13:36:23 +0530

    Revert "fix: import doctor api debug"
    This reverts commit 99483bc32748243a8ca6c8d0294e3f75083c8508.

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -379,8 +379,6 @@ const importDoctorsFromNMC = async (req, res) => {
         for (let i = 0; i < nmcDoctors.length; i++) {
             const doc = nmcDoctors[i];
 
-            console.log(doc);
-
             try {
                 // Example mapping (adjust based on your API)
                 const randomStr = Math.random().toString(36).substring(2, 8);
@@ -399,8 +397,6 @@ const importDoctorsFromNMC = async (req, res) => {
                         ?.trim()
                         .replace(/\s+/g, " ");
                 };
-
-                console.log(email)
 
                 const user = await userModel.createUser({
                     name: cleanName(doc?.name || ""),
